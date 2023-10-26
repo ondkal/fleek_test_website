8214bf2d4ba480ec68eeff568a4fc8368f7cb46f
Ondrej Kalny <ondrej@kalny.name>
2023-10-26 11:12:57 -0400

    Added initial files.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Fleek.co test website</title>
+  <meta name="description" content="Fleek.co test website">
+  <meta name="author" content="ok">
+
+</head>
+
+<body>
+
+<?php echo date("Y-m-d--H-i-s"); ?>
+
+</body>
+</html>
